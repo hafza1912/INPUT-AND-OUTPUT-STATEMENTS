c138c9dd3788c3e7cfcb5af68f81bbc4e246b1f4
hafza1912 <hafzaazath@gmail.com>
2025-11-07 11:43:05 +0530

    Delete d5 q2.cpp

--- d5 q2.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-	int a,b,c,d,r,h;
-	scanf("%d%d%d%d%d%d",&a,&b,&c,&d,&r,&h);
-	printf("Area of rectangle is%d",a*b);
-	printf("Area of square is%d",a*a);
-	printf("Area of triangle is%d",(b*h)/2);
-	printf("Area of circle is%d",(22/7)*r*r);
-}
