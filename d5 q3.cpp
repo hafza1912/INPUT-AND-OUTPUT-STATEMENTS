036025d276da0e05f0364e7d36a394d2a307d73d
hafza1912 <hafzaazath@gmail.com>
2025-11-07 11:43:15 +0530

    Delete d5 q3.cpp

--- d5 q3.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<math.h>
-int main(){
-	int x1,x2,y1,y2;
-	scanf("%d%d%d%d",&x1,&x2,&y1,&y2);
-	float dist;
-	dist=sqrt(pow(x2-x1,2)+pow(y2-y1,2));
-	printf("%f",dist);
-}
